67b5e211a7f3eb0e0b89a7bd3326c6c4138683cf
Takuya Kondo <takondo@microsoft.com>
2022-12-09 10:37:59 -0800

    Added check for objects without AES keys

--- Check-11Bissues.ps1
+++ Check-11Bissues.ps1
@@ -4,17 +4,19 @@ $noSET = New-Object -TypeName 'System.Collections.ArrayList'
 $badSET = New-Object -TypeName 'System.Collections.ArrayList'
 $rc4only = New-Object -TypeName 'System.Collections.ArrayList'
 $AESonlyDC = New-Object -TypeName 'System.Collections.ArrayList'
+$NoAESKeys = New-Object -TypeName 'System.Collections.ArrayList'
 
-$computers = Get-ADComputer -filter * -Properties msDS-SupportedEncryptionTypes,operatingSystem,operatingSystemVersion,userAccountControl
-$users = Get-ADUser -Filter * -Properties msDS-supportedEncryptionTypes,servicePrincipalName 
+$computers = Get-ADComputer -filter * -Properties msDS-SupportedEncryptionTypes, operatingSystem, operatingSystemVersion, userAccountControl, passwordLastSet
+$users = Get-ADUser -Filter * -Properties msDS-supportedEncryptionTypes, servicePrincipalName, passwordLastSet
+$dateAESadded = (Get-ADGroup "Read-only Domain Controllers" -Properties WhenCreated).WhenCreated
  
-foreach($computer in $computers) {
-    if(!$computer.Enabled) {continue}
+foreach ($computer in $computers) {
+    if (!$computer.Enabled) { continue }
 
     #Look for legacy Windows OS's (pre 2008/Vista)
-    if($computer.operatingSystem -match "Windows") {
+    if ($computer.operatingSystem -match "Windows") {
         [int]$version = ($computer.OperatingSystemVersion.Split("."))[0]
-        if($version -lt 6) {
+        if ($version -lt 6) {
             Write-Host "*****************************************"
             Write-Host "Legacy OS detected: " -NoNewline
             Write-Host $computer -ForegroundColor Red
@@ -24,7 +26,7 @@ foreach($computer in $computers) {
     }
 
     #Look for computer objects with msDS-SupportedEncryptionTypes not configured
-    if(!$computer.'msDS-SupportedEncryptionTypes') {
+    if (!$computer.'msDS-SupportedEncryptionTypes') {
         $noSET.Add($computer) | Out-Null
     }
     else {
@@ -32,55 +34,63 @@ foreach($computer in $computers) {
 
         #Look for computer objects with msDS-SupportedEncryptionTypes configured but no etype enabled
         #Example: only CompoundIdentity is set
-        if(($set -band 0x1F) -eq 0) {
+        if (($set -band 0x1F) -eq 0) {
             $badSET.Add($computer) | Out-Null
         }
         #Keep track of objects where AES is disabled
-        elseif(($set -band 0x18) -eq 0) {
+        elseif (($set -band 0x18) -eq 0) {
             $rc4only.Add($computer) | Out-Null
         }
 
         #Look for DCs with msDS-SupportedEncryptionTypes RC4 disabled
-        if($computer.'userAccountControl' -band 0x2000 -and ($set -band 0x4) -eq 0) {
+        if ($computer.'userAccountControl' -band 0x2000 -and ($set -band 0x4) -eq 0) {
             $AESonlyDC.Add($computer) | Out-Null
         }
     }
+
+    if ($computer.passwordlastset -le $dateAESadded) {
+        $NoAESKeys.Add($computer)
+    }
 }
 
-foreach($user in $users) {
-    if(!$user.Enabled) {continue}
+foreach ($user in $users) {
+    if (!$user.Enabled) { continue }
 
-    if($user.servicePrincipalName) {
+    if ($user.servicePrincipalName) {
         #ignore krbtgt
-        if($user.Name -match "krbtgt") {continue}
+        if ($user.Name -match "krbtgt") { continue }
         
         #This is most likely a service account
         #Look for objects with msDS-SupportedEncryptionTypes not configured
-        if(!($user.'msDS-SupportedEncryptionTypes')) {
+        if (!($user.'msDS-SupportedEncryptionTypes')) {
             $noSET.Add($user) | Out-Null
         }
         else {
             $set = $user.'msDS-SupportedEncryptionTypes'
 
             #Look for objects with msDS-SupportedEncryptionTypes configured but no etype enabled
-            if(($set -band 0x1F) -eq 0) {
+            if (($set -band 0x1F) -eq 0) {
                 $badSET.Add($user) | Out-Null
             }
             #Keep track of objects where AES is disabled
-            elseif(($set -band 0x18) -eq 0) {
+            elseif (($set -band 0x18) -eq 0) {
                 $rc4only.Add($user) | Out-Null
             }
         }
     }
+
+    if ($user.passwordlastset -le $dateAESadded) {
+        $NoAESKeys.Add($user)
+    }
 }
 
 Write-Host "======================================" 
-if($noSET.Cout -ne 0) {
+if ($noSET.Cout -ne 0) {
     Write-Host "There are $($noSET.Count) objects that do not have msDS-SupportedEncryptionTypes configured."
     Write-Host "When authenticating to this target, Kerberos will use the setting of DefaultDomainSupportedEncTypes registry on the authenticating DC to determinte supported etypes."
     Write-Host "This defaults to a value of 0x27, which means 'use AES for session keys and RC4 for ticket encryption'"
     Write-Host "If this target server does not support AES, please set msDS-SupportedEncryptionTypes to 4 on this object so that only RC4 is used."
-    foreach($obj in $noSET) {
+    foreach ($obj in $noSET) {
         Write-Host "`t"$obj
     }
 }
@@ -91,12 +101,12 @@ else {
 
 
 Write-Host "======================================"
-if($badSET.Count -ne 0) {
+if ($badSET.Count -ne 0) {
     Write-Host "There are $($badSET.Count) objects that have msDS-SupportedEncryptionTypes configured, but no encryption protocol is allowed."
     Write-Host "This can cause authentication to/from this object to fail."
     Write-Host "Please either delete the existing msDS-SupportedEncryptionTypes settings, or add supported etypes."
     Write-Host "Example: Add 0x1C to signify support for AES128, AES256, and RC4"
-    foreach($obj in $badSET) {
+    foreach ($obj in $badSET) {
         Write-Host "`t"$obj
     }
 }
@@ -104,33 +114,44 @@ else {
     Write-Host "There were no objects with msDS-SupportedEncryptionTypes configured without any etypes enabled."
 }
 
+Write-Host "======================================"
+if ($NoAESKeys.Count -ne 0) {
+    Write-Host "There are $($NoAESKeys.Count) objects that do not have AES Keys generated. This can occur if the account's password has not been changed after adding Server 2008 or newer DCs"
+    Write-Host "Authentication to this target can fail if AES is required by either the client or the KDC."
+    Write-Host "Please change/reset the accounts' password, and AES keys will be automatically generated." 
+    foreach ($obj in $NoAESKeys) {
+        Write-Host "`t"$obj
+    }
+}
+else {
+    Write-Host "There were no accounts whose passwords predate AES capabilities."
+}
 
-if($rc4only.Count -ne 0) {
-    Write-Host "======================================"
+Write-Host "======================================"
+if ($rc4only.Count -ne 0) {
     Write-Host "There are $($rc4only.Count) computers/services that are configured for RC4/DES only."
-    Write-Host "If you have any clients or DCs that are configured to only support AES, authentication will fail."
+    Write-Host "Authentication to this target can fail if AES is required by either the client or the KDC."
     Write-Host "Here is the list of objects that are RC4/DES only:"
-    foreach($obj in $rc4only) {
+    foreach ($obj in $rc4only) {
         Write-Host "`t"$obj
     }
 
     Write-Host "`nA common scenario where authentication fails after installing November 2022 update or newer on DCs in this condition is if DCs are configured to only support AES"
     Write-Host "Example: Setting the 'Configure encryption types allowed for Kerberos' policy to AES only on DCs"
-    if($AESonlyDC.Count -eq 0) {
+    if ($AESonlyDC.Count -eq 0) {
         Write-Host "No DCs were detected that are configured for AES only"
     }
     else {
         Write-Host "Here are the DCs configured for AES only:"
-        foreach($obj in $AESonlyDC) {
+        foreach ($obj in $AESonlyDC) {
             Write-Host "`t"$obj
         }
     }
 }
 
 
-
-if($badSET.Count -eq 0 -and $noSET.Count -eq 0 -and $rc4only.Count -eq 0) {
+if ($badSET.Count -eq 0 -and $noSET.Count -eq 0 -and $rc4only.Count -eq 0) {
     Write-Host "======================================"
-    Write-Host "Configurations known to cause Kerberos authentication failures after installing November 2022 update or newer on DCs were not detected."
+    Write-Host "Configurations known to cause Kerberos authentication failures after installing November 2022 update or newer on DCs were not detected." -ForegroundColor Green -BackgroundColor Black
     Write-Host "Pleae contact Microsoft Support if you do see any failures."
 }
